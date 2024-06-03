0a08be40e577763501988767d3b7b9bdea2f4381
SaravanaKV <sk9341748@gmail.com>
2024-06-03 17:35:38 +0530

    hw

--- shw66.py
+++ shw66.py
@@ -13,3 +13,23 @@ Parse the input list one element at a time. Convert to integer, push it to a sta
 Whenever you see an
 operator, pop two elements from the stack, apply the operation and push the result back.
 '''
+def operation(expression):
+    inList=[]
+    for element in expression:
+        if element in "+-*/":
+            num2=inList.pop()
+            num1=inList.pop()
+            if element=='+':
+                inList.append(num1+num2)
+            elif element=='-':
+                inList.append(num1-num2)
+            elif element=='*':
+                inList.append(num1*num2)
+            elif element=='/':
+                inList.append(num1//num2)
+        else:
+            inList.append(int(element))
+    return inList.pop()
+expression=["10", "2", "3", "+","-", "5", "*"]
+result=operation(expression)
+print(result)
