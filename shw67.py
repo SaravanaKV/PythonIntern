491d5f9dd4b10de8380a569f9262b51cf969a83c
SaravanaKV <sk9341748@gmail.com>
2024-06-21 17:23:50 +0530

    Expression

--- shw67.py
+++ shw67.py
@@ -8,3 +8,28 @@ input ["-","10", "+", "2", "3", "*", "5"]
 output =  25
 explanation (10 - ( 2 + 3)) * 5 = 25
 '''
+
+def operation(expression):
+    inList=[]
+    operatorList=[]
+    for element in expression:
+        if element in "+-*/":
+            operatorList.append(element)
+        else:
+            inList.append(int(element))
+            if len(inList) == 2:
+                num2 = inList.pop()
+                num1 = inList.pop()
+                operator=operatorList.pop()
+                if operator == '+':
+                    inList.append(num1 + num2)
+                elif operator == '-':
+                    inList.append(num1 - num2)
+                elif operator == '*':
+                    inList.append(num1 * num2)
+                elif operator == '/':
+                    inList.append(num1 // num2)
+    return inList.pop()
+expression=["+","1", "2", "*", "5"]
+result=operation(expression)
+print(result)
